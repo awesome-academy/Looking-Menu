71f44389280f7b217fd051cced3b761b88e13bb6
thanduchuy <thanduchuyz299@gmail.com>
2020-12-29 08:42:34 +0700

    create layout project and config extension,id and apikey

---
+++ Thực Đơn/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  extensions.swift
+//  Thực Đơn
+//
+//  Created by Huy Than Duc on 27/12/2020.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func cornerCircle() {
+        self.layer.cornerRadius = self.frame.height/2
+    }
+    func getImageFromURL(imgURL:String) {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            let url = URL(string: imgURL)
+            let data = try? Data(contentsOf: url!)
+            DispatchQueue.main.async { [weak self] in
+                self!.image = UIImage(data: data!)
+            }
+        }
+    }
+}
+
+extension UIView {
+    func addShadowView(radius: CGFloat) {
+        if let black = UIColor(named: "black") {
+            self.layer.shadowColor = black.cgColor
+            self.layer.shadowOpacity = 0.3
+            self.layer.shadowOffset = CGSize.zero
+            self.layer.shadowRadius = radius
+        }
+    }
+}
